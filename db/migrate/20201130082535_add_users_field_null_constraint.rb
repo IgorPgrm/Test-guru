6ad24b4a3c9585e603a07f809dbd3c714014a539
IgorShum <Shumeikoigor@yandex.ru>
2020-11-30 11:38:53 +0300

    Установлены значения по умолчанию

---
+++ db/migrate/20201130082535_add_users_field_null_constraint.rb
@@ -0,0 +1,7 @@
+class AddUsersFieldNullConstraint < ActiveRecord::Migration[6.0]
+  def change
+    change_column_null(:users, :email, false)
+    change_column_null(:users, :first_name, false)
+    change_column_null(:users, :last_name, false)
+  end
+end
