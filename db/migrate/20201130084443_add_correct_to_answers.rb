2e5de9fa4b9a33651e63f3feff2083677286b2be
IgorShum <Shumeikoigor@yandex.ru>
2020-11-30 12:16:57 +0300

    +атрибут correct для Answer

---
+++ db/migrate/20201130084443_add_correct_to_answers.rb
@@ -0,0 +1,6 @@
+class AddCorrectToAnswers < ActiveRecord::Migration[6.0]
+  def change
+    add_column(:answers, :correct, :boolean)
+    change_column_default(:answers, :correct, false)
+  end
+end
