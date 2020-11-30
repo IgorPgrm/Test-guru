55b3d72d10ae001f4159aa9d1fedcfd9da7e5cf0
IgorShum <Shumeikoigor@yandex.ru>
2020-11-30 11:02:20 +0300

    Применена миграция, Категории

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_074943) do
+ActiveRecord::Schema.define(version: 2020_11_30_075930) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.text "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
