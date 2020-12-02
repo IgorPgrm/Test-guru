d4f948d3c85530ab54d0b0c9b20815704de0f03b
IgorShum <Shumeikoigor@yandex.ru>
2020-12-02 22:36:00 +0300

    Методы в модели

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,5 @@
 class Test < ApplicationRecord
+  def show_by_category(category)
+    #Test.where()
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def list_of_tests(level=0)
+    #User.find(id: self.id)
+  end
 end
