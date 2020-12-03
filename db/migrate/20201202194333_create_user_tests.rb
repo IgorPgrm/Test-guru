bf35f6b10ea116b99303d9790002acb89aa5341f
IgorShum <Shumeikoigor@yandex.ru>
2020-12-03 15:47:44 +0300

    +Создание таблицы тесты пользователя

--- db/migrate/20201202194333_create_user_tests.rb
+++ db/migrate/20201202194333_create_user_tests.rb
@@ -1,6 +1,8 @@
 class CreateUserTests < ActiveRecord::Migration[6.0]
   def change
     create_table :user_tests do |t|
+      t.belongs_to :test
+      t.belongs_to :user
 
       t.timestamps
     end
