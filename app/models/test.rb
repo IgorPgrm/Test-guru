178eb6949904e12581e25a2fce38c34704f91834
IgorShum <Shumeikoigor@yandex.ru>
2020-12-03 15:48:46 +0300

    +Метод сортировки названия тестов по убыванию

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,9 @@
 class Test < ApplicationRecord
-  def show_by_category(category)
-    #Test.where()
+  has_many :user_tests
+  has_many :users, through: :user_tests
+  belongs_to :category
+
+  def self.show_by_category(category)
+    Test.joins(:category).where("categories.title = ?", category).order("tests.title DESC")
   end
 end
