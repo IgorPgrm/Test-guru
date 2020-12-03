5d75b529a8d3fdeba70c80bf9f0e5be6433a467b
IgorShum <Shumeikoigor@yandex.ru>
2020-12-03 15:47:00 +0300

    +Добавлены ассоциации в модели

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-  def list_of_tests(level=0)
-    #User.find(id: self.id)
+  has_many :user_tests
+  has_many :tests, through: :user_tests
+
+  def list_of_tests(level = 0)
+    Test.joins(:user_tests).where("user_id = ? and level= ?", self.id, level)
   end
 end
--- app/models/user_test.rb
+++ app/models/user_test.rb
@@ -1,2 +1,4 @@
 class UserTest < ApplicationRecord
+  has_many :tests
+  has_many :users
 end
