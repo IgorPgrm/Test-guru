f4d931f677c01021e5473de3db9ac1c9605b62fc
IgorShum <Shumeikoigor@yandex.ru>
2020-12-06 00:01:10 +0300

    Ассоциация для тест.автор

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,7 +5,7 @@ class Test < ApplicationRecord
   has_many :user_tests
   has_many :users, through: :user_tests
   belongs_to :category
-  belongs_to :user
+  belongs_to :author, :class_name => "User"
 
   def self.show_by_category(category)
     Test.joins(:category).where('categories.title = ?', category).order('tests.title DESC')
