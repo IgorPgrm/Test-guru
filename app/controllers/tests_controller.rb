ea20892d85e0f9be6fb89f2fd5cd0a07a35639e0
IgorShum <Shumeikoigor@yandex.ru>
2020-12-12 11:01:56 +0300

    +Контроллер для теста +Вьюха для теста Роуты

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,7 +1,28 @@
-
 class TestsController < ApplicationController
-
   def index
+    render plain: Test.all.inspect
+  end
 
+  def new
+
+  end
+
+  def show
+    redirect_to root_path
+  end
+
+  def create
+    test = Test.create(test_params)
+    render plain: test.inspect
+  end
+
+  def search
+    result = ["Class:#{params.class}", "Parameters:#{params.inspect}"]
+    render plain: result.join("\n")
+  end
+
+  private
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'tests#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/tests/:id/start', to: 'tests#start'
+
+  get '/tests/:category/:title', to: 'tests#search'
+
+  resources :tests do
+    resources :questions, shallow: true
+  end
+
 end
