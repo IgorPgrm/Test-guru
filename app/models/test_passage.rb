d11faeedc13e4f0e32272359c19ee51fe6697bee
IgorShum <Shumeikoigor@yandex.ru>
2020-12-18 00:20:33 +0300

    Назначение следующего вопроса, обратный вызов

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,13 +4,13 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_save :set_next_question
 
   def accept!(answer_ids)
     if correct_answer?(answer_ids)
       self.correct_questions += 1
     end
 
-    self.current_question = next_question
     save!
   end
 
@@ -35,5 +35,9 @@ class TestPassage < ApplicationRecord
 
   def next_question
     test.questions.order(:id).where('id > ?', current_question.id).first
+  end
+
+  def set_next_question
+    self.current_question = next_question
   end
 end
