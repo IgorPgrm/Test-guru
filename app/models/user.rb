b2933f000931e89a6d0365330eec7e39e486718f
IgorShum <Shumeikoigor@yandex.ru>
2020-12-24 23:42:22 +0300

    +gem Devise install

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
-require 'digest/sha1'
 
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
 
   has_many :authored_tests, foreign_key: "author_id", class_name: "Test"
   has_many :test_passages
   has_many :tests, through: :test_passages
-
-  has_secure_password
 
   def list_of_tests(level = 0)
     tests.where(level: level)
