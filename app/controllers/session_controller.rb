31f3759482be3713ea02b6fd84f7be4c1783ce56
IgorShum <Shumeikoigor@yandex.ru>
2020-12-24 13:58:46 +0300

    Исправления

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,14 +6,10 @@ class SessionController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     redirect_path = cookies[:redirect_path]
+    cookies.delete(:redirect_path)
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      if redirect_path.present?
-      cookies.delete(:redirect_path)
-      redirect_to redirect_path
-      else
-        redirect_to tests_path
-      end
+      redirect_to redirect_path || tests_path
     else
       flash.now[:alert] = 'Are you a Guru? Verify your e-mail and password please'
       render :new
