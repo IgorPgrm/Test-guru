39511bc8740ec2d6d805aad5198f82fe1da789cc
IgorShum <Shumeikoigor@yandex.ru>
2020-12-25 21:17:32 +0300

    +Перенаправление админа на админ-панель

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,13 @@
 class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_record_not_found
 
+
+
   private
+
+  def after_sign_in_path_for(resource)
+    resource.is_a?(Admin) ? admin_tests_path : tests_path
+  end
 
   def rescue_record_not_found
     render plain: "#{self.controller_name.capitalize!} with (id=#{self.params[:id]}) was not found"
