b8530082ee8a422b8c75746abc7bbb583ab9299a
IgorShum <Shumeikoigor@yandex.ru>
2021-01-11 23:15:25 +0300

    +GitHubService Отправляет вопрос с вариантами ответа в GitGist

--- lib/clients/git_hub_client.rb
+++ lib/clients/git_hub_client.rb
@@ -1,8 +1,17 @@
 class GitHubClient
   ROOT_ENDPOINT = 'https://api.github.com'.freeze
+  ACCESS_TOKEN = '4ea5bd2ea5a916f6f089066784c79476c6d8deef'.freeze
 
   def initialize
     @http_client = setup_http_client
+  end
+
+  def create_gist(params)
+    @http_client.post('gists') do |request|
+      request.headers['Authorization'] = "token #{ACCESS_TOKEN}"
+      request.headers['Content-Type'] = 'application/json'
+      request.body = params.to_json
+    end
   end
 
   private
