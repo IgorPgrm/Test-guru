ff8b44824d9e1d00039ecb3babf8ad873c158c93
IgorShum <Shumeikoigor@yandex.ru>
2021-01-21 17:39:00 +0300

    >Изменен адаптер бд на postgres >Изменены timestamp миграций >замена на банг метод в seed для вызова исключений +Добавлены params для devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,17 @@
 
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def default_url_options
     I18n.default_locale == I18n.locale ? {} : { lang: I18n.locale }
   end
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+  end
 
   def after_sign_in_path_for(resource)
     flash[:notice] = "Hello! #{current_user.first_name} #{current_user.last_name}!"
