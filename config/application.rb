2f2e830f99a28f9196ee3a71bbf7808d66cae918
IgorShum <Shumeikoigor@yandex.ru>
2021-01-22 23:35:54 +0300

    add i18n config

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module TestGuru
     config.load_defaults 6.0
     config.time_zone = 'St. Petersburg'
     config.i18n.default_locale = :ru
+    config.i18n.fallbacks = true
+    config.i18n.fallbacks = %i[ru en]
     config.autoload_paths << "#{Rails.root}/lib/clients"
 
     # Settings in config/environments/* take precedence over those specified here.
