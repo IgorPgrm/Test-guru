a6ebfc411896b305b2262c50cee83ed4fe5a7d3f
IgorShum <Shumeikoigor@yandex.ru>
2021-01-24 19:20:04 +0300

    >переписан feedback_controller v3

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -3,6 +3,6 @@ class FeedbackMailer < ApplicationMailer
     @subject = subject
     @text = text
     @sent_at = sent_at
-    mail to: "Shumeikoigor@yandex.ru"
+    mail to: "Shumeikoigor@yandex.ru", from: "ecececep@gmail.com"
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,8 @@
 # Load the Rails application.
 require_relative 'application'
 
+ActionMailer::Base.delivery_method = :sendmail
+ActionMailer::Base.smtp_settings = { :domain  => 'localhost' }
+
 # Initialize the Rails application.
 Rails.application.initialize!
