108ab20a99b1f31b58429a7903472b1e5acb8d0e
IgorShum <Shumeikoigor@yandex.ru>
2021-01-24 18:56:20 +0300

    >переписан feedback_controller v2

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,8 +1,8 @@
 class FeedbackMailer < ApplicationMailer
   def send_feedback_email(subject, text, sent_at = Time.now)
     @subject = subject
-    @sent_on = sent_at
     @text = text
-    @headers = {content_type: 'text/html'}
+    @sent_at = sent_at
+    mail to: "Shumeikoigor@yandex.ru"
   end
 end
