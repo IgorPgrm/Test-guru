c90e8ad2908b2c4bbfd8763937af8bf4a8dc17a4
IgorShum <Shumeikoigor@yandex.ru>
2021-01-24 17:57:55 +0300

    >feedback_send

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users, path: :gurus, path_names: {sign_in: :login, sign_out: :logout}
   root to: 'tests#index'
-  post 'send_feedback', to: 'feedback#feedback_send'
+  post 'feedback_send', to: 'feedback#feedback_send'
   get 'feedback', to: 'feedback#index'
 
   resources :tests, only: :index do
