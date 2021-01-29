5162ea4e4e7121718b77e19907b83996b875762a
IgorShum <Shumeikoigor@yandex.ru>
2021-01-29 22:05:20 +0300

    Контроллер достижений картинки перемещены в каталог

--- app/controllers/admin/achievements_controller.rb
+++ app/controllers/admin/achievements_controller.rb
@@ -1,6 +1,32 @@
 class Admin::AchievementsController < ActionController::Base
   layout "admin"
+  before_action :find_achievement, only: %i[edit update]
+  before_action :available_image, only: %i[new create edit update]
+
   def index
-    @ach = Achievement.all
+    @achievements = Achievement.all
+  end
+
+  def new
+    @achievement = Achievement.new
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+
+  def find_achievement
+    @achievement = Achievement.find(params[:id])
+  end
+
+  def available_image
+    @images = Dir.glob(Rails.application.assets.paths[1] + "/achievements/*.png").select { |e| File.file? e }
+    @images.map! {|f| f.split("/").last}
   end
 end
